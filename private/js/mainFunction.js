9d66e7ac3222a1f707639e66e4e65e9bc372865b
Man Sun <msun45@my.bcit.ca>
2021-12-18 16:34:02 -0800

    add ieee functions

--- private/js/mainFunction.js
+++ private/js/mainFunction.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
+
     function calculateIEEE() {
-        var decimal = document.getElementById("ieee-decimal-input").value;
-        console.log(decimal);
+        var number = document.getElementById("ieee-decimal-input").value;
+        var result = new floatContext(number, 4, 5);
+        console.log(result);
     }
     
     document.getElementById("ieee-decimal-submit").onclick = function() {
